49fa7009d3f111ea492cfa0942997d8155ba5f64
Piton <vppiton@gmail.com>
2023-12-22 13:59:17 +0100

    Start debugging

--- src/VAE.py
+++ src/VAE.py
@@ -2,6 +2,7 @@
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 # import torch.distributions as distrib
 # import torchvision
 # import matplotlib
@@ -16,23 +17,24 @@ class AE(nn.Module):
         super(AE, self).__init__()
         
         self.encoder = nn.Sequential(
-            Conv2DLayerEncoder(in_channels, in_channels*2, 2, 0, 0, 2, 0, 0),
+            Conv2DLayerEncoder(in_channels, in_channels*2, 2, 1, 0, 2, 1, 0),
             Conv2DLayerEncoder(in_channels*2, in_channels*4, 2, 1, 0, 2, 1, 0),
-            Conv2DLayerEncoder(in_channels*4, in_channels*8, 2, 0, 0, 2, 0, 0),
+            Conv2DLayerEncoder(in_channels*4, in_channels*8, 2, 1, 0, 2, 1, 0),
             Conv2DLayerEncoder(in_channels*8, in_channels*16, 2, 1, 0, 2, 1, 0),
-            Conv2DLayerEncoder(p=0.2)
+            nn.Dropout2d(p=0.2)
         )
         
         self.decoder = nn.Sequential(
             Conv2DLayerDecoder(in_channels*16, in_channels*8, 2, 1, 0, 2),
-            Conv2DLayerDecoder(in_channels*8, in_channels*4, 2, 0, 0, 2),
+            Conv2DLayerDecoder(in_channels*8, in_channels*4, 2, 1, 0, 2),
             Conv2DLayerDecoder(in_channels*4, in_channels*2, 2, 1, 0, 2),
-            Conv2DLayerDecoder(in_channels*2, in_channels, 2, 0, 0, 2),
+            Conv2DLayerDecoder(in_channels*2, in_channels, 2, 1, 0, 2),
             nn.Dropout2d(p=0.2)
             )
 
     def forward(self, x):
         encoded = self.encoder(x)
+        encoded = F.upsample(encoded, scale_factor=1.2, mode='bilinear')
         decoded = self.decoder(encoded)
         return decoded
     
@@ -61,13 +63,14 @@ class Conv2DLayerDecoder(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size_conv, 
                  stride_conv, padding_conv, up_scale):
         super(Conv2DLayerDecoder, self).__init__()
-        self.upsample = nn.Upsample(scale_factor=up_scale, mode='bilinear')
+        #self.upsample = nn.Upsample(scale_factor=up_scale, mode='bilinear')
         self.conv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size_conv, 
                               stride_conv, padding_conv)
         self.batchnorm = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
+        #x = self.upsample(x)
         x = self.conv(x)
         x = self.batchnorm(x)
         x = self.relu(x)
@@ -77,20 +80,21 @@ class Conv2DLayerDecoder(nn.Module):
 
 class VAE(AE):
     
-    def __init__(self):
-        super(VAE, self).__init__(latent_dims)
+    def __init__(self, latent_dims):
+        super(VAE, self).__init__()
         self.mu = nn.Sequential(
-            nn.Linear(1, 
-                      latent_dims), 
+            nn.Linear(6400, latent_dims), 
             nn.ReLU()
         )
-        self.sigma = nn.Sequential(nn.Linear(self.encoding_dims, 
-                                             self.latent_dims),
+        self.sigma = nn.Sequential(nn.Linear(6400, latent_dims),
                                    nn.Softplus())
+        self.img_from_sample = nn.Sequential(nn.Linear(latent_dims, 6400),
+                                            nn.ReLU())
         
     def encode(self, x):
         
         x_hidden = self.encoder(x)
+        x_hidden = torch.reshape(x_hidden, (64, -1))
         mu = self.mu(x_hidden)
         sigma = self.sigma(x_hidden)
         
@@ -104,13 +108,36 @@ class VAE(AE):
         z_params = self.encode(x)
         # Obtain latent samples and latent loss
         z_tilde, kl_div = self.latent(x, z_params)
+        z_tilde = self.img_from_sample(z_tilde)
+        z_tilde = torch.reshape(z_tilde, (64, 16, 20, 20))
+        # Upsample
+        z_tilde = F.upsample(z_tilde, scale_factor=1.2, mode='bilinear')
         # Decode the samples
         x_tilde = self.decode(z_tilde)
-        return x_tilde.reshape(-1, 1, 28, 28), kl_div
+        return x_tilde, kl_div
     
     def latent(self, x, z_params):
         z = z_params[0] + torch.randn(z_params[0].shape[-1]) * z_params[1]
-        kl_div = 1/2*torch.sum(1 + torch.log((z_params[1])**2) - (z_params[0])**2 - (z_params[1])**2)
+        kl_div = 1/2*torch.sum(1 + torch.log((z_params[1])**2) - 
+                               (z_params[0])**2 - (z_params[1])**2)
         
         return z, kl_div
+
+
+def train_VAE(model, dataloader, epochs=5, lr=1e-3):
+    optimizer = torch.optim.Adam(model.parameters(), lr)
+    criterion = torch.nn.MSELoss(reduction='sum')
     
+    for epoch in range(1, epochs + 1):
+        full_loss = torch.Tensor([0])
+        for i, (x, _) in enumerate(dataloader):
+            if (x.shape[0]!=64): # CHECK DATALOADER SIZE
+                pass
+            loss = criterion(model(x)[0], x)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            full_loss += loss
+        print(full_loss)
+        
+    return model
--- temp.py
+++ temp.py
@@ -5,12 +5,23 @@ import torch
 from src.dataset_loader import *
 from src.VAE import *
 
+import matplotlib.pyplot as plt
+
 print(os.getcwd())
 root = os.getcwd()
 batch_size = 64
 train_dataloader, test_dataloader =  MNIST_give_dataloader(root, batch_size)
 
-model = AE
+model = VAE(10)
 
-x, y = next(iter(train_dataloader))
-xbar = model(x)
+epochs = 5
+lr = 1e-3
+model = train_VAE(model, train_dataloader, epochs, lr)
+
+x, _ = next(iter(train_dataloader))
+xbar, kldiv = model(x)
+
+with torch.no_grad():
+    plt.imshow(x[0].reshape(28,28), cmap="gray")
+    plt.figure()
+    plt.imshow(xbar[0].reshape(28,28), cmap="gray")
