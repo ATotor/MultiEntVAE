Fixed Kl div

scripts/main.py
@@ -25,7 +25,9 @@ parser.add_argument('--lr', type=float, default=1e-3)
 parser.add_argument('--batch_size', type=int, default=16)
 parser.add_argument('--no-disp', action="store_false")
 parser.add_argument('--no-save', action="store_false")
-parser.add_argument('--load', action="store_true", help="Load model instead of training")
+parser.add_argument('--load-recent', action="store_true", help="Load most recent model instead of training")
+parser.add_argument('--load', type=str, default="", help="Load specified model instead of training")
+
 args = parser.parse_args()
 
 epochs = args.epochs
@@ -33,19 +35,20 @@ lr = args.lr
 batch_size = args.batch_size
 disp = args.no_disp
 save = args.no_save
+load_recent = args.load_recent
 load = args.load
 beta = args.beta
 
 transform = nn.Sequential(
     T.MelSpectrogram(sample_rate=16000,n_mels=256,n_fft=2048,norm='slaney'),
-    Norm()
+    Normalize()
 
 ).to(device)
 inverse_transform = nn.Sequential(
     T.InverseMelScale(sample_rate=16000,n_mels=256,n_stft=2048 // 2 + 1,norm="slaney"),
     #T.GriffinLim(n_fft=2048),
     librosa_GriffinLim(n_fft=2048),
-    Norm(),
+    Normalize(),
 ).to(device)
 
 
@@ -58,15 +61,20 @@ print(f"Number of parameters : {n_params:}")
 log_dir = "logs/" + datetime.now().strftime("%Y%m%d-%H%M%S")
 writer = SummaryWriter(log_dir) if disp else None
 
-if load is False :
+if not load and load_recent is False:
     print("Training model")
     model, loss = train_VAE(model, train_dataloader, beta, epochs, lr,device,writer)    
-else:
+elif load_recent:
     print("Loading model")
     model = load_model(find_most_recent_VAE())
     model = model.to(device)
     loss = None
-        
+elif load:
+    print("Loading model")
+    model = load_model(load)
+    model = model.to(device)
+    loss = None    
+
 if save:
     save_model(model)
     #if loss:    save_loss(loss)
@@ -74,4 +82,4 @@ if save:
 if disp:
     #if loss :     disp_loss(loss)
     #disp_MNIST_example(model, test_dataloader)
-    tensorboard_writer(model,test_dataloader,writer,inverse_transform,device)
+    tensorboard_writer(model,test_dataloader,writer,inverse_transform,args)

src/VAE.py
@@ -27,7 +27,7 @@ class AE(nn.Module):
             LayerDecoder(in_channels//8, in_channels//4, kernel_size_conv=3,stride_conv=2,padding_conv=1,output_padding_conv=0),
             LayerDecoder(in_channels//4, in_channels//2, kernel_size_conv=3,stride_conv=2,padding_conv=1,output_padding_conv=0),
             LayerDecoder(in_channels//2, in_channels, kernel_size_conv=3,stride_conv=2,padding_conv=1,output_padding_conv=0),
-            nn.Dropout1d(p=0.2)
+            #nn.Dropout1d(p=0.2)
             )
 
     def forward(self, x):
@@ -114,29 +114,35 @@ class VAE(AE):
         # Encode the inputs
         mu, sigma = self.encode(x)
         # Obtain latent samples
-        z = self.latent(mu, sigma)
+        z, kl_div = self.latent(mu, sigma)
         # Decode the samples
         x_tilde = self.decode(z)
-        return x_tilde
+        return x_tilde, kl_div
     
     def latent(self, mu, sigma):
         device = self.dummy_param.device
         z = mu + sigma*torch.randn_like(sigma,device=device)
-        return z
+        kl_div = 0.5*(1 + torch.log(sigma**2) - mu**2 - sigma**2).sum()
+        return z, kl_div
 
+def compute_loss(model, x, beta):
+    recons_criterion = torch.nn.MSELoss(reduction='sum')
+    x_tilde, kl = model(x)
+    full_loss = recons_criterion(x_tilde,x) - beta * kl
+    
+    return full_loss
 
 def train_VAE(model, dataloader, beta = 1, epochs=5, lr=1e-3, device = torch.device("cpu"),writer=None):
     optimizer = torch.optim.Adam(model.parameters(), lr)
-    criterion_1 = torch.nn.MSELoss(reduction='sum')
-    criterion_2 = torch.nn.KLDivLoss(reduction='sum')
+    # criterion_1 = torch.nn.MSELoss(reduction='sum')
+    # criterion_2 = torch.nn.KLDivLoss(reduction='sum')
     loss_tensor = torch.tensor([]).to(device)
     
     for epoch in range(1, epochs + 1):
         full_loss = torch.Tensor([0]).to(device)
-        for i, item in enumerate(dataloader):
+        for _, item in enumerate(dataloader):
             x = item['x']
-            x_tilde = model(x)
-            loss = criterion_1(x_tilde, x) + beta * criterion_2(x_tilde, x)
+            loss = compute_loss(model,x,beta)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()

src/plots.py
@@ -25,26 +25,29 @@ def disp_MNIST_example(model, dataloader):
             ax[1,i].imshow(xbar[i].reshape(28,28), cmap="gray")
     plt.show()
 
-def tensorboard_writer(model, dataloader,writer,inverse_transform,device):
+def tensorboard_writer(model, dataloader,writer,inverse_transform,args):
     print("Creating logs")
+    writer.add_text(f"beta",str(args.beta))
+    writer.add_text(f"learning rate",str(args.lr))
     model.eval()
     with torch.no_grad():
         item = next(iter(dataloader))
         spec = item['x']
         batch_size = spec.shape[0]
-        init_audio = inverse_transform(spec)
+        
         images = spec[:,None,:,:]
         
-        output = model(spec)
+        output, _ = model(spec)
         output_image = output[:,None,:,:]
-        gen_audio = inverse_transform(output)
-
+        
         for batch_number in range(batch_size):
+            init_audio = inverse_transform(spec[batch_number])
+            gen_audio = inverse_transform(output[batch_number])
             writer.add_image(f'{batch_number}/Initial Spectrogram {item["fname"][batch_number]}',images[batch_number])
             writer.add_image(f'{batch_number}/Generated Spectrogram {item["fname"][batch_number]}',output_image[batch_number])
             writer.add_audio(f'{batch_number}/Initial audio {item["fname"][batch_number]}',item["audio"][batch_number],sample_rate = 16000)
-            writer.add_audio(f'{batch_number}/Initial recreated audio {item["fname"][batch_number]}',init_audio[batch_number],sample_rate = 16000)
-            writer.add_audio(f'{batch_number}/Generated audio {item["fname"][batch_number]}',gen_audio[batch_number],sample_rate = 16000)
+            writer.add_audio(f'{batch_number}/Initial recreated audio {item["fname"][batch_number]}',init_audio,sample_rate = 16000)
+            writer.add_audio(f'{batch_number}/Generated audio {item["fname"][batch_number]}',gen_audio,sample_rate = 16000)
 
             # for i in range(len(gen_audio[batch_number])): 
             #     writer.add_scalar(f"{batch_number}/Generated Waveform",gen_audio[batch_number][i],i)

src/utils.py
@@ -43,11 +43,13 @@ def save_loss(loss):
     date_time = datetime.now().strftime("%d-%m-%Y_%H_%M")
     np.save(os.path.join("results","loss_"+date_time), loss)
 
-class Norm(nn.Module):
+class Normalize(nn.Module):
     def __init__(self):
-        super(Norm, self).__init__()
+        super(Normalize, self).__init__()
     def forward(self, x):
-        return x/x.max()
+        minval = x.min()
+        maxval = x.max()
+        return (x - minval) / (maxval - minval)
     
 class librosa_GriffinLim(nn.Module):
     def __init__(self,n_fft=2048):